878609e86199e49b2a3286afbb59358ffaff783a
pierelucas <pierelucas@keemail.me>
2019-09-22 13:00:59 +0200

    pyscan

--- pyscan.py
+++ pyscan.py
@@ -4,6 +4,7 @@
 import socket
 import subprocess
 import sys
+from time import localtime
 from datetime import datetime
 
 # Klasse
@@ -13,20 +14,39 @@ class PortScanner_Init():
     def __init__(self):
 
         # Zeitmessung
+        lt = localtime()
+        self.jahr, self.monat, self.tag = lt[0:3]
+        self.stunde, self.minute, self.sekunde = lt[3:6]
+
         self.zeitbeginn = bytes()
         self.zeitende = bytes()
         self.zeitgesamt = bytes()
-        self.zeitallgemein = bytes()
 
         # Daten
         self.remoteServer = []
         self.remoteServerIP = bytes()
 
+        # Zähler
+        self.count = 0
+
     # Ausgabe
     def __str__(self):
+
         return "-" * 60 + "\n" \
                + "Please wait, scanning remtoe host: " + self.remoteServerIP \
                + "\n" + "-" * 60
+
+    def ausgabe(self, x):
+
+        if x == 0:
+            print(f"Datum: {portscanner_init.tag:02d}.{portscanner_init.monat:02d}.{portscanner_init.jahr:4d}")
+            print(f"Uhrzeit: {portscanner_init.stunde:02d}:{portscanner_init.minute:02d}:{portscanner_init.sekunde:02d}")
+            print(portscanner_init)
+        elif x == 1:
+            print()
+            print('Scanning Completed in: ', portscanner_init.zeitgesamt)
+            print(f"Datum: {portscanner_init.tag:02d}.{portscanner_init.monat:02d}.{portscanner_init.jahr:4d}")
+            print(f"Uhrzeit: {portscanner_init.stunde:02d}:{portscanner_init.minute:02d}:{portscanner_init.sekunde:02d}")
 
     # Eingabe
     def eingabe(self):
@@ -38,22 +58,24 @@ class PortScanner_Init():
         inputs = 1
         while inputs != 0:
             try:
+                print()
                 self.remoteServer = input("Enter a remote host to scan: ")
                 self.remoteServerIP = socket.gethostbyname(self.remoteServer)
                 inputs = 0
             except KeyboardInterrupt:
+                print()
                 print("You presses Ctrl+C")
                 sys.exit()
             except RuntimeError:
+                print()
                 print("Wrong Statement")
             except ValueError:
+                print()
                 print("You must enter a valid Host")
 
     # Methode zur Ermittlung von Zeitwerten
     def zeit(self, x):
 
-        if x == 0:
-            self.zeitallgemein = datetime.now()
         if x == 1:
             self.zeitbeginn = datetime.now()
         elif x == 2:
@@ -64,6 +86,7 @@ class PortScanner_Init():
     def banner(self):
         print("-" * 60)
         print("Python Portscanner")
+        print("Author: PiereLucas")
         print("-" * 60)
 
 
@@ -85,44 +108,51 @@ class PortScanner(PortScanner_Init):
     def scan(self):
 
         try:
-            # Portscan der Ports 1 - 35535
+            # Portscan der Ports 1 - 1024
             for self.port in range(1, 1025):
                 self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.result = self.sock.connect_ex((self.remoteServerIP, self.port))
                 if self.result == 0:
+                    print("<-")
                     print("Port {}: 	 Open".format(self.port))
+                    print("->")
+                    self.count += 1
                     self.open = self.port
                 else:
                     print("Scanning Port: ", self.port)
                 if self.port == 1024:
+                    print()
+                    print("Results:", self.count, "Open Ports found")
+                    print("-" * 60)
                     print("Port {}: 	 Open".format(self.open))
+                    print("-" * 60)
                 self.sock.close()
 
         except KeyboardInterrupt:
+            print()
             print("You presses Ctrl+C")
             sys.exit()
 
         except socket.gaierror:
+            print()
             print("Hostname could not be resolved. Exiting")
             sys.exit()
 
         except socket.error:
+            print()
             print("Couldn't connect to server")
             sys.exit()
 
 
-# Klassenvariable
+# Instanzobjekte
 portscanner_init = PortScanner_Init()
 portscanner = PortScanner()
-
 
 # Eingabe
 portscanner_init.eingabe()
 
 # Ausgabe
-portscanner_init.zeit(0)
-print(portscanner_init.zeitallgemein)
-print(portscanner_init)
+portscanner_init.ausgabe(0)
 
 # Prüfe wann der Scan startet
 portscanner_init.zeit(1)
@@ -137,4 +167,4 @@ portscanner_init.zeit(2)
 portscanner_init.zeit(3)
 
 # Gebe die Zeitinformation aus
-print('Scanning Completed in: ', portscanner_init.zeitgesamt)
+portscanner_init.ausgabe(1)
